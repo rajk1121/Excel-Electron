37e2a7efc3d6c02c2afbb9be1fdf84e167e2ae18
rajk1121 <rajat.mishra1121@gmail.com>
2020-10-27 12:16:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const $  =require('jquery')
+
+$(document).ready(function(){
+    $('.rows > .cell').on('click', function(){
+        // console.log(this)
+        // console.log(($(this).attr('row')))
+        let rid = parseInt($(this).attr('row'));
+        let cid = parseInt($(this).attr('col'));
+        let colAddr = String.fromCharCode(65 + cid);
+        let rowAddr = rid + 1;
+        $('.address-container').val(colAddr+rowAddr)
+        console.log(colAddr+rowAddr)
+    })
+})
